bb184cacb1430dbaa7714026a95d3fe572938070
WillOrmsby/DonCheadleX <78742980+WilliamOrmsby@users.noreply.github.com>
2021-04-08 23:18:45 -0400

    Update CCT.c

--- CCT.c
+++ CCT.c
@@ -15,30 +15,7 @@ int main(void)
     char msg[] = "HTTP/1.1 200 OK\r\n"
 	"Content-Type: text/html; charset=UTF-8\r\n\r\n"
        	"<!DOCTYPE html>"
-	"<html>"
-	"<head>"
-	"<title>The Rock</title>"
-	"</head>"
-	"<style>"
-	".otext{ text-align: center; position: relative; font-family: Verdana, sans-serif; font-size: 20px;}"
-	"h1{text-align: center; color: #FFFFFF;text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);}"
-	".tracking-in-expand{animation:tracking-in-expand 2.1s cubic-bezier(.215,.61,.355,1.000) both}"
-	"@keyframes tracking-in-expand{0%{letter-spacing:-.5em;opacity:0}40%{opacity:.6}100%{opacity:1}}"
-	"@keyframes rotate-center{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}"
-	".rotate-center{animation:rotate-center .6s ease-out both}"
-	"body { background: linear-gradient(268deg, #a6bb29 0%, #00ffc5 100%);}"
-	".container{ position: relative; height:640; width:960; background-image: url(https://specials-images.forbesimg.com/imageserve/5f5ebdfe1a1834b803586841/960x0.jpg) height=320px width=480px;}"
-	"</style>"
-	"<body><div class=""tracking-in-expand"">"
-	"<h1 style =""color: #FFFFFF;text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);color: #FFFFFF; text-align: center;"">Do You Smell What He is Cooking?</h1></div>"
-	"<h3 style = ""text-align: center; font-family;Monaco"">The Rock</h3>"
-	"<p style = ""text-align: center; text-indent: 15px;"">The Rock is the 8th in A line of great men that includes<br> the like of Abraham Lincoln, Paddington Bear, and Magneto.</p>"
-	"<img src=""https://static.wikia.nocookie.net/prowrestling/images/a/ad/Wwe_the_rock_png_by_double_a1698_day9ylt-pre_%281%29.png"">"
-	"<h4 style=""text-align: center; font: 10px Verdana; transition: all 4s linear;"">Hi I am the Rock, and I am cooking.</h4>"
-	"<h3 style=""text-align: center;"">Fun Facts about the Rock</h3>"
-	"<ul><li> He is not Actually a Rock, He is a Meteorite</li><li> He Cooks with Food Normal Humans Can't Smell</li><li> Yes</li></ul>"
-	"<div class=""container"">Hello I am cooking!</div>"
-	"</body></html>";
+	///INSERT HTML HERE\\\\;
 
     int sock = socket(AF_INET, SOCK_STREAM, 0), one = 1;
     if (sock == -1)
